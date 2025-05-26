cbcb849aebfc60c677e1b7437b99fb86eb1cc228
Jean Mendes da Silva <jeanmds7809@gmail.com>
2025-05-26 17:20:56 -0300

    Inicio da responsividade

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,28 +16,34 @@ export default function Home() {
 
         </section>
 
-        <button onClick={showMenu} className="fixed z-20 mt-4 ml-5">
+        <button onClick={showMenu} className="fixed z-20 mt-4 ml-5 xl:hidden">
             <img src="/menu.svg" alt="" className="h-16"/>
         </button>
 
-        <header className="flex w-screen bg-black fixed items-center justify-center">
+        <header className="flex w-screen bg-black fixed items-center justify-center xl:justify-between xl:px-44">
             <div>
               <a href="/">
                 <img src="/logo.svg" alt="" className="h-24" />
               </a>
             </div>
+
+            <div className="text-white max-xl:hidden text-lg font-semibold flex gap-16">
+              <a href="/">Home</a>
+              <a href="Instituicoes">Instituições</a>
+              <a href="Formulario">Formulário</a>
+            </div>
         </header>
 
         <div className="bg-[#E6E1D3] pt-24 flex flex-col items-center">
-            <h1 className="font-bold text-3xl py-8">O que é o projeto?</h1>
+            <h1 className="font-bold text-3xl py-8 xl:text-6xl xl:py-14">O que é o projeto?</h1>
 
-            <p className="px-10 text-justify pb-10">Neste site, reunimos diversas instituições que oferecem serviços comunitários voltados para imigrantes em situação de vulnerabilidade. Nosso objetivo é facilitar o acesso a informações importantes e confiáveis. Aqui, você encontrará uma lista de organizações que prestam apoio social, jurídico, psicológico e outros tipos de assistência. Você poderá consultar os serviços oferecidos, os endereços, formas de contato e outros detalhes essenciais para que possa buscar ajuda com segurança e dignidade. Tudo isso está disponível de forma acessível e organizada, em um só lugar.</p>
+            <p className="px-10 text-justify pb-10 xl:text-2xl xl:px-24 xl:pb-14">Neste site, reunimos diversas instituições que oferecem serviços comunitários voltados para imigrantes em situação de vulnerabilidade. Nosso objetivo é facilitar o acesso a informações importantes e confiáveis. Aqui, você encontrará uma lista de organizações que prestam apoio social, jurídico, psicológico e outros tipos de assistência. Você poderá consultar os serviços oferecidos, os endereços, formas de contato e outros detalhes essenciais para que possa buscar ajuda com segurança e dignidade. Tudo isso está disponível de forma acessível e organizada, em um só lugar.</p>
         </div>
 
         <div className="bg-black flex flex-col items-center text-white">
-            <h1 className="font-bold text-3xl py-8">Quem nós somos?</h1>
+            <h1 className="font-bold text-3xl py-8 xl:text-6xl xl:py-14">Quem nós somos?</h1>
 
-            <p className="px-10 text-justify pb-10">Este projeto foi desenvolvido por nós, estudantes universitários da área de tecnologia, como parte de uma iniciativa acadêmica com foco no impacto social. Fomos desafiados a criar uma solução digital que contribuísse com a comunidade, e optamos por reunir, em um só lugar, informações úteis para imigrantes em situação de vulnerabilidade. Acreditamos no poder da informação e da solidariedade. Por isso, este site foi pensado com responsabilidade, empatia e compromisso social. Esperamos que ele seja uma ponte entre quem precisa de ajuda e quem pode oferecê-la. Caso queira ver sua instituição registrada no site, clique <span className="text-[#00A8FF]"><a href="/Formulario">aqui</a></span>.</p>
+            <p className="px-10 text-justify pb-10 xl:text-2xl xl:px-24 xl:pb-14">Este projeto foi desenvolvido por nós, estudantes universitários da área de tecnologia, como parte de uma iniciativa acadêmica com foco no impacto social. Fomos desafiados a criar uma solução digital que contribuísse com a comunidade, e optamos por reunir, em um só lugar, informações úteis para imigrantes em situação de vulnerabilidade. Acreditamos no poder da informação e da solidariedade. Por isso, este site foi pensado com responsabilidade, empatia e compromisso social. Esperamos que ele seja uma ponte entre quem precisa de ajuda e quem pode oferecê-la. Caso queira ver sua instituição registrada no site, clique <span className="text-[#00A8FF]"><a href="/Formulario">aqui</a></span>.</p>
         </div>
         
     </section>
